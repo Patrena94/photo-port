1bc05b1555c21ab4f34ce6f5cbf039382bae5f4d
Vashti <patrena@sbcglobal.net>
2021-09-26 07:23:54 -0700

    updated gallery, about, Navigation, and App

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,21 +1,34 @@
-import React from 'react';
-import { capitalizeFirstLetter} from '../../utils/helpers';
-import photo from "../../assets/small/commercial/0.jpg";
+import React, { useState } from 'react';
+import Nav from './components/Nav';
+import About from './components/About';
+import Gallery from './components/Gallery';
 
-function Gallery(props){
-    const currentCategory ={
-        name:"commerical",
-        description: "Photos of grocery stores, food trucks, and other commerical projects",
-    };
-    return (
-        <section>
-            <h1>{capitalizeFirstLetter(currentCategory.name)}</h1>
-            <p>{currentCategory.name}</p>
-        <div>
-            <img src={photo}
-            />
-        </div>
-        </section>
-    );
+function App() {
+  const [categories] = useState([
+    {
+      name: 'commercial',
+      description: 'Photos of grocery stores, food trucks, and other commercial projects',
+    },
+    { name: 'portraits', description: 'Portraits of people in my life' },
+    { name: 'food', description: 'Delicious delicacies' },
+    { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
+  ]);
+
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
+  return (
+    <div>
+      <Nav
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+      ></Nav>
+      <main>
+        <Gallery currentCategory={currentCategory}></Gallery>
+        <About></About>
+      </main>
+    </div>
+  );
 }
-export default Gallery;
+
+export default App;
